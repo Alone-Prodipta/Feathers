33517ac0dd375d8d8529faa3f1a1d2dc06c29c0f
Prodipta <nayebprodipta@gmail.com>
2026-05-12 16:56:44 +0530

    customizing the landing page

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+var modal = document.getElementById('id01');
+var overlay = document.getElementById('overlay');
+var closeBtn = document.querySelector('.close');
+
+function closeModal() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+}
+
+closeBtn.onclick = closeModal;
+overlay.onclick = closeModal;
