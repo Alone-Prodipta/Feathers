9f022f6f4f90839485c9499bd714d6a409a073ff
Prodipta <nayebprodipta@gmail.com>
2026-06-24 19:51:01 +0530

    changed the color

--- contact.php
+++ contact.php
@@ -118,7 +118,7 @@
                 <form class="d-flex w-50" action="brain.php" method="GET" role="search">
                     <input name="search" class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
                     value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
-                    <button class="btn btn-outline-success " type="submit"><a href="cart.php" style="color: white; text-decoration: none;">Cart</a></button>
+                    <button class="btn btn-outline-success " type="submit"><a href="cart.php" style="color: green; text-decoration: none;">Cart</a></button>
                 </form>
             </div>
         </div>
